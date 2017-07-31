84f4c2ba0ffac725c8d4747e1e86a24678d05fd9
Nathan Elg <elgn@students.wwwu.edu>
2017-07-31 17:41:53 -0600

    things got fixed i think

--- app/components/game/game.js
+++ app/components/game/game.js
@@ -11,6 +11,88 @@
     // This is a freebie we are using the GameService to help keep our controller clean. The GameServie will be in charge of creating and shuffling the deck.
     gc.deck = GameService.getDeck()
 
+    var firstCard;
+    var secondCard;
+
+    gc.attempts = 0
+    gc.totalMatches = 0
+
+    gc.victory = false
+
+    
+    gc.selectCard = function(card){
+      if(card.show == true || firstCard && secondCard){
+        return
+      }
+      card.show = true
+      if(!firstCard){
+        firstCard = card;
+      }else{
+        secondCard = card
+        
+        var match = isMatch(firstCard, secondCard)
+        if(match){
+          resetCards();
+        }else {
+          $timeout(function(){
+
+          firstCard.show = false
+          secondCard.show = false
+          resetCards()
+          }, 700)
+        }
+      }
+    }
+  
+
+
+    resetCards = function (card) {
+
+      firstCard = null
+
+      secondCard = null
+
+      gc.attempts += 1
+      }
+
+
+      isMatch = function (firstCard, secondCard) {
+        if( firstCard.title == secondCard.title ){
+            gc.totalMatches += 1;
+            checkVictory()
+            return true
+
+        }else{
+
+            return false
+        }
+
+      }
+
+      checkVictory = function(){
+        if(gc.totalMatches == 12){
+          gc.victory = true
+        }
+
+      }
+
+      gc.resetGame = function(){
+        gc.deck = GameService.getDeck();
+        gc.attempts = 0;
+        gc.totalMatches = 0;
+        gc.victory = false
+
+      }
+
+      
+
+
+
+
+
+   
+
+
     // Create two card variables. These will be responsible
     // for keeping track of our selections as we click cards.
 
